b6011b48e7e933dbf2af23e73f56908544718bc0
ssongnb <95738249+ssongnb@users.noreply.github.com>
2021-12-10 14:33:44 +0900

    am

---
+++ j20211210/Comment.java
@@ -0,0 +1,18 @@
+//패키지선언
+package j20211210;
+
+
+
+
+public class Comment {
+    
+        
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+
+
+    
+    
+}
---
+++ j20211210/Variable.java
@@ -0,0 +1,17 @@
+package j20211210;
+
+public class Variable {
+    public static void main(String[] args) {
+        
+        //자바에서는 변수 선언시 반드시 자료형을 함께 선언해야만 한다.
+
+        int a = 5;
+        String b = "Lunch";
+
+        System.out.println(a);
+
+
+
+
+    }
+}
